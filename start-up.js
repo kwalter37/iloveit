c1ac0af608b5de8f611d80bcc47b8c622c56ea3d
Kevin Walter <kwalter@Kevins-MacBook-Pro.local>
2016-08-10 17:01:57 -0700

    adding mongodb

--- start-up.js
+++ start-up.js
@@ -1,21 +1,87 @@
-const http = require('http');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+var PRODUCTS_COLLECTION = "products";
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  //res.setHeader('Content-Type', 'text/plain');
-  //res.end('Hello Ku!\n');
-  var products = [
-  	{brand: "Ragu", name: "Chunky Tomato and Onion", rating: 9},
-  	{brand: "X", name: "Mushroom", rating: 1},
-  	{brand: "Y", name: "Tomato and Basil", rating: 2}
-  ];
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(products));
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 3000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/products"
+ *    GET: finds all products
+ *    POST: creates a new contact
+ */
+
+app.get("/products", function(req, res) {
+  db.collection(PRODUCTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
+
+app.post("/products", function(req, res) {
+  var newProduct = req.body;
+  newProduct.createDate = new Date();
+
+  //if (!(req.body.firstName || req.body.lastName)) {
+  //  handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  //}
+
+  db.collection(PRODUCTS_COLLECTION).insertOne(newProduct, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new product.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+/*  "/products/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+//app.get("/products/:id", function(req, res) {
+//});
+
+//app.put("/products/:id", function(req, res) {
+//});
+
+//app.delete("/products/:id", function(req, res) {
+//});
